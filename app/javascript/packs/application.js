b585e30ef623731e0bc1537015d9145bb735b6ab
SizweMalobola <25reyals@gmail.com>
2021-10-07 13:25:30 +0200

    fixed bootstarp bug by installing it via -- yarn Install

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,16 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import "bootstrap";
+//= require jquery3
+//= require popper
+//= require bootstrap
+
 import Rails from "@rails/ujs";
 import Turbolinks from "turbolinks";
 import * as ActiveStorage from "@rails/activestorage";
 import "channels";
+
+import "bootstrap";
 
 Rails.start();
 Turbolinks.start();
