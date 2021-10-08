af1e14681d09c9ae87beef9df54e78b8ab5eaab0
SizweMalobola <25reyals@gmail.com>
2021-10-08 17:53:27 +0200

    user's create method is functional, users can be created in the database by submitting a form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,17 @@ class UsersController < ApplicationController
     end
     
     def create
+        @user = User.new(user_params);
+        if @user.save
+            flash[:success] = 'successfully created a new user'
+            redirect_to posts_path
+        else
+            render :new
+        end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:name,:email,:password,:password_confirmation)
     end
 end
