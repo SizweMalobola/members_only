20b6f601e66dabc81fd233ef4aa010448a654f33
SizweMalobola <25reyals@gmail.com>
2021-10-10 12:59:57 +0200

    sessions can now be created

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,14 @@ class SessionsController < ApplicationController
     end
     
     def create
+        user = User.find_by(email: params[:session][:email].downcase)
+        if user && user.authenticate(params[:session][:password])
+            helpers.login(user)
+            redirect_to posts_path
+        else
+            flash.now[:danger] = 'Invalid email/password combination'
+            render :new
+        end
     end
 
     def destroy
