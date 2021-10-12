c1e0512b2f0aa2d5543db1ebcec68bd7507d5ae2
SizweMalobola <25reyals@gmail.com>
2021-10-12 20:00:20 +0200

    set up a before filter to check whether a user is logged in before they are allowed to create a new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+    before_action :logged_in_user, only: [:new, :create]
+
     def home
     end
 
@@ -27,5 +29,14 @@ class PostsController < ApplicationController
     private
     def post_params
         params.require(:post).permit(:title,:body)
+    end
+
+    # confirms a logged-in user.
+    def logged_in_user
+        unless helpers.logged_in?
+            helpers.store_location
+            flash[:danger] = 'Please login'
+            redirect_to login_path
+        end
     end
 end
