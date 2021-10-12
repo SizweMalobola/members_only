f40fad6f8380f5560951b82c23f5ff8cedf53d63
SizweMalobola <25reyals@gmail.com>
2021-10-12 13:38:01 +0200

    defined post crud methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,31 @@
 class PostsController < ApplicationController
     def home
     end
-    def index    
+
+    def index
+        @posts = Post.all
+    end
+
+    def show
+        @post = Post.find(params[:user_id])
+    end
+
+    def new
+        @post = Post.new
+    end
+
+    def create
+        @post = Post.new(post_params)
+        if @post.save
+            flash[:success] = 'Post was successful'
+            redirect_to @post
+        else
+            render :new
+        end
+    end
+
+    private
+    def post_params
+        params.require(:post).premit(:title,:body)
     end
 end
