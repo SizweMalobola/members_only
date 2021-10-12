98f9e02217be96b37713cfd73630e71a98c57895
SizweMalobola <25reyals@gmail.com>
2021-10-12 13:34:04 +0200

    added :show route to posts resource

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     get '/login', to: 'sessions#new'
     post '/login', to: 'sessions#create'
     delete '/logout', to: 'sessions#destroy'
-    resources :posts, only: [:new, :create, :index]
+    resources :posts, only: [:new, :create, :index, :show]
     resources :users, only: [:new, :create]
     # resources :sessions, only: [:new, :create,:destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
